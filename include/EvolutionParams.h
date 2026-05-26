5b7a8311ab3120b04ec44b96337656acc0a3d870
Mateusz <mateusz222244@op.pl>
2026-05-26 17:51:23 +0200

    feat: evolution params init

--- include/EvolutionParams.h
+++ include/EvolutionParams.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cmath>
 
 struct EvolutionParams {
     double alpha; // Mnożnik populacji
@@ -6,13 +7,22 @@ struct EvolutionParams {
     double gamma; // Prawdopodobieństwo krzyżowania
     double delta; // klonowanie
 
+    double mutationRate;   // %
+
     int epsilon;        // Warunek stopu (ile pokoleń bez poprawy znosimy)
     int maxGenerations; // Twardy limit pokoleń
     int maxTreeDepth;   // Głębokość drzewa
 
-    double populationSize; // Pi
-    double numClones;      // f
-    double numMutations;   // omega
-    double numCrossovers;  // ksi
-    double mutationRate;   // %
+    int populationSize; // Pi: Całkowita liczebność pokolenia (alpha * num_tasks * num_resources)
+    int numClones;      // f: Liczba osobników przechodzących bez zmian (delta * Pi)
+    int numMutations;   // omega: Liczba osobników losowo modyfikowanych (beta * Pi)
+    int numCrossovers;  // ksi: Liczba osobników do krzyżowania (gamma * Pi)
+
+    void setup(int taskCount, int processorCount) {
+        populationSize = static_cast<int>(std::round(alpha * taskCount * processorCount));
+
+        numClones = static_cast<int>(std::round(delta * populationSize));
+        numMutations = static_cast<int>(std::round(beta * populationSize));
+        numCrossovers = static_cast<int>(std::round(gamma * populationSize));
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
+#include "EvolutionParams.h"
 #include "FunctionType.h"
 
 #include <iostream>
 
 int main() {
-    std::cout << static_cast<int>(FunctionType::COUNT);
+    EvolutionParams params{};
+    params.alpha = 5.0;
+    params.beta = 0.1;  // % mutacji
+    params.gamma = 0.6; // % krzyzowania
+    params.delta = 0.3; // % klony
+    params.mutationRate = 0.05;
+
+    params.maxGenerations = 100;
+    params.epsilon = 20;
+    params.maxTreeDepth = 10;
+
+    int numTasks = 12;
+    int numProcessors = 4;
+
+    params.setup(numTasks, numProcessors);
+
     return 0;
 }
